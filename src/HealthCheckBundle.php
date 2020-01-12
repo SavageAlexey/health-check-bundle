d255ac8c17bc75633a7abc701757a9c483d156e5
Aleksey Alieksieienko <nikolayevich.aleksey@isoftbet.com>
2020-01-12 18:56:12 +0200

    HC; Prepare bundle for autoconfigure while including it into the project

--- src/HealthCheckBundle.php
+++ src/HealthCheckBundle.php
@@ -2,8 +2,17 @@
 
 namespace niklesh\HealthCheckBundle;
 
+use niklesh\HealthCheckBundle\DependencyInjection\Compiler\HealthServicesPath;
+use niklesh\HealthCheckBundle\Service\HealthInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class HealthCheckBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+        $container->addCompilerPass(new HealthServicesPath());
+        $container->registerForAutoconfiguration(HealthInterface::class)->addTag(HealthInterface::TAG);
+    }
 }
--- src/Service/HealthInterface.php
+++ src/Service/HealthInterface.php
@@ -6,6 +6,8 @@ use niklesh\HealthCheckBundle\Entity\HealthDataInterface;
 
 interface HealthInterface
 {
+    const TAG = 'health.service';
+
     public function getName(): string;
     public function getHealthInfo(): HealthDataInterface;
 }
